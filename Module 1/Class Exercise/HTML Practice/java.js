8dec770a223a13ba9a00d492d9f684b37f639ed6
tlecocq99 <tlecocq99@gmail.com>
2025-04-06 22:48:20 -0500

    Removed these to change folder name

--- Module 1/Class Exercise/HTML Practice/java.js
+++
@@ -1,7 +0,0 @@
-//Toggle icon and setting the display to be hidden
-function TestsFunction() {
-  var T = document.getElementById("TestsDiv"),
-    displayValue = "";
-  if (T.style.display == "") displayValue = "none";
-  T.style.display = displayValue;
-}
