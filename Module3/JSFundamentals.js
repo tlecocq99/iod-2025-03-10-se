d039d2eca08a96a924f8121d12e63d0d65ab3c84
ClearChoicePools <thomas.soccerdude@gmail.com>
2025-04-19 11:17:48 -0500

    added json

---
+++ Module3/JSFundamentals.js
@@ -0,0 +1,21 @@
+//Question #1
+const main = () => {
+  "" + 1 + 0;
+  "" - 1 + 0;
+  true + false;
+  !true;
+  6 / "3";
+  "2" * "3";
+  4 + 5 + "px";
+  "$" + 4 + 5;
+  "4" - 2;
+  "4px" - 2;
+  " -9 " + 5;
+  " -9 " - 5;
+  null + 1;
+  undefined + 1;
+  undefined == null;
+  undefined === null;
+  " \t \n" - 2;
+};
+console.log(main());
