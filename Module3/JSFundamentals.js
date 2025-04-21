58717a8405553957c3c905567d5facd5e3536f13
Thomas Lecocq <tlecocq99@gmail.com>
2025-04-21 00:30:25 -0500

    Q9

--- Module3/JSFundamentals.js
+++ Module3/JSFundamentals.js
@@ -161,3 +161,59 @@ basketballGame
   .basket()
   .threePointer()
   .fullTime();
+
+/*Question #8: The object below represents a single city.
+a) Write a function that takes an object as an argument and uses a for...in loop to access
+and print to the console each of those object properties and their values. Test it using
+the sydney object below.
+b) Create a new object for a different city with different properties and call your function
+again with the new object. */
+const sydney = {
+  name: "Sydney",
+  population: 5_121_000,
+  state: "NSW",
+  founded: "26 January 1788",
+  timezone: "Australia/Sydney",
+};
+for (let key in sydney) {
+  console.log("key: " + key);
+  console.log("value: " + sydney[key]);
+}
+
+const melbourne = {
+  name: "Melbourne",
+  population: 4_000_000,
+  state: "VIC",
+  founded: "1788",
+  timezone: "Australia/Melbourne",
+};
+for (let key in melbourne) {
+  console.log("key: " + key);
+  console.log("value: " + melbourne[key]);
+}
+
+/*Question #9: Use the following variables to understand how JavaScript stores objects by reference.
+a) Create a new moreSports variable equal to teamSports and add some new sport
+values to it (using push and unshift)
+b) Create a new dog2 variable equal to dog1 and give it a new value
+c) Create a new cat2 variable equal to cat1 and change its name property
+d) Print the original teamSports, dog1 and cat1 variables to the console. Have they
+changed? Why?
+e) Change the way the moreSports and cat2 variables are created to ensure the
+originals remain independent. */
+let teamSports = ["Hockey", "Cricket", "Volleyball"];
+let moreSports = teamSports.slice(); // Create a shallow copy of the array
+moreSports.push("Baseball", "Soccer");
+moreSports.unshift("Tennis", "Rugby");
+console.log(moreSports);
+const dog1 = "Bingo";
+let dog2 = dog1; // This creates a new variable dog2 but does not change dog1
+console.log(dog2);
+dog2 = "Rover";
+console.log(dog2);
+let cat1 = { name: "Fluffy", breed: "Siberian" };
+const cat2 = { ...cat1, name: "Whiskers" }; // Create a shallow copy of the object
+console.log(cat2);
+console.log(teamSports);
+console.log(dog1); //The original variables have not changed as shallow copies were created for the array and object.
+console.log(cat1);
