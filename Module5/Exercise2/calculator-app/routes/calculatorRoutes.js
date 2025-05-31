8cadda9ab3e097f2b099d1dac33045bd009e6cf7
Thomas Lecocq <tlecocq99@gmail.com>
2025-05-31 12:33:03 -0500

    added all 4 calc functions with routes

--- Module5/Exercise2/calculator-app/routes/calculatorRoutes.js
+++ Module5/Exercise2/calculator-app/routes/calculatorRoutes.js
@@ -10,4 +10,32 @@ router.get("/add", (req, res) => {
   res.status(200);
   res.json({ result: sum });
 });
+
+router.get("/subtract", (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let sum = number1 - number2;
+  console.log(sum);
+  res.status(200);
+  res.json({ result: sum });
+});
+
+router.get("/multiply", (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let sum = number1 * number2;
+  console.log(sum);
+  res.status(200);
+  res.json({ result: sum });
+});
+
+router.get("/divide", (req, res) => {
+  let number1 = parseInt(req.query.num1);
+  let number2 = parseInt(req.query.num2);
+  let sum = number1 / number2;
+  console.log(sum);
+  res.status(200);
+  res.json({ result: sum });
+});
+
 module.exports = router;
