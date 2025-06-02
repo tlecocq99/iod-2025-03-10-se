f4f126eec967f4a0fe470b91d357ccc4cd96aded
Thomas Lecocq <tlecocq99@gmail.com>
2025-06-01 23:41:01 -0500

    changed some file names and added app.js folder and rerouted accordingly

--- Module5/Exercise2-3-5/calculator-app/app.js
+++ Module5/Exercise2-3-5/calculator-app/app.js
@@ -1,13 +1,11 @@
-const calculatorRoutes = require("./routes/calculatorRoutes");
 const express = require("express");
 const app = express();
 const port = 3000;
+
+const calculatorRoutes = require("./routes/calculatorRoutes");
 
 app.use("/calculator", calculatorRoutes);
 
 app.use("/", express.static("public"));
 
-app.listen(port, () => {
-  console.log(`Example app listening
-at http://localhost:${port}`);
-});
+module.exports = app;
--- Module5/Exercise2-3-5/calculator-app/controllers/calculatorController.js
+++ Module5/Exercise2-3-5/calculator-app/controllers/calculatorController.js
@@ -1,41 +1,37 @@
-const express = require("express");
-const router = express.Router();
-
-// new route for adding two numbers
-router.get("/add", (req, res) => {
+const addNumbers = (req, res) => {
   let number1 = parseInt(req.query.num1);
   let number2 = parseInt(req.query.num2);
   let sum = number1 + number2;
   console.log(sum);
   res.status(200);
   res.json({ result: sum });
-});
+};
 
-router.get("/subtract", (req, res) => {
+const subtractNumbers = (req, res) => {
   let number1 = parseInt(req.query.num3);
   let number2 = parseInt(req.query.num4);
   let sum = number1 - number2;
   console.log(sum);
   res.status(200);
   res.json({ result: sum });
-});
+};
 
-router.get("/multiply", (req, res) => {
+const multiplyNumbers = (req, res) => {
   let number1 = parseInt(req.query.num5);
   let number2 = parseInt(req.query.num6);
   let sum = number1 * number2;
   console.log(sum);
   res.status(200);
   res.json({ result: sum });
-});
+};
 
-router.get("/divide", (req, res) => {
+const divideNumbers = (req, res) => {
   let number1 = parseInt(req.query.num7);
   let number2 = parseInt(req.query.num8);
   let sum = number1 / number2;
   console.log(sum);
   res.status(200);
   res.json({ result: sum });
-});
+};
 
-module.exports = router;
+module.exports = { addNumbers, subtractNumbers, multiplyNumbers, divideNumbers };
