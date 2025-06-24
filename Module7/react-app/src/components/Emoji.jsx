2cacc69fb9ec1fc13ffea0629334d2f1018f7cd2
Thomas Lecocq <tlecocq99@gmail.com>
2025-06-24 00:41:39 -0500

    added styling edits to the emoji component - increased IconButton size

--- Module7/react-app/src/components/Emoji.jsx
+++ Module7/react-app/src/components/Emoji.jsx
@@ -6,12 +6,22 @@ import MoodIcon from "@mui/icons-material/Mood";
 export default function Emoji() {
   const { emoji, toggleMood } = useContext(EmojiContext);
   return (
-    <Box display="flex" alignItems="center" justifyContent="center" mb={2}>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      mb={2}
+      mt={4}
+    >
       <Typography variant="h2" component="span" mr={1}>
         {emoji}
       </Typography>
-      <IconButton onClick={toggleMood} color="primary">
-        <MoodIcon />
+      <IconButton
+        style={{ color: "inherit" }}
+        onClick={toggleMood}
+        color="primary"
+      >
+        <MoodIcon size="large" />
       </IconButton>
     </Box>
   );
