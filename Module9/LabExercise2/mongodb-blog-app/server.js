9418003fb8afb07bdefa0dccfbe63546cd935e75
Thomas Lecocq <tlecocq99@gmail.com>
2025-07-20 01:49:35 -0500

    implementation of mongoDB API

---
+++ Module9/LabExercise2/mongodb-blog-app/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const dbConnect = require("./dbConnect");
+const userRoutes = require("./routes/userRoutes");
+
+// parse requests of content-type - application/json
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my MongoDB application." });
+});
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
