03c087ee799f5af4f2ffd78ddb4f455400b3e057
Thomas Lecocq <tlecocq99@gmail.com>
2025-07-20 21:12:05 -0500

    added in routes for post, comment and like

--- Module9/LabExercise2/mongodb-blog-app/server.js
+++ Module9/LabExercise2/mongodb-blog-app/server.js
@@ -3,16 +3,24 @@ const express = require("express");
 const app = express();
 const dbConnect = require("./dbConnect");
 const userRoutes = require("./routes/userRoutes");
+const postRoutes = require("./routes/postRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+const likeRoutes = require("./routes/likeRoutes");
 
 // parse requests of content-type - application/json
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to my MongoDB application." });
 });
+
+// Link routes
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-
-app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
