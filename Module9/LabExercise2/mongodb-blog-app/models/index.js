800200860acfc606c0eee0bcb25ec5842f869f26
Thomas Lecocq <tlecocq99@gmail.com>
2025-07-20 21:23:45 -0500

    edited to have all exports

--- Module9/LabExercise2/mongodb-blog-app/models/index.js
+++ Module9/LabExercise2/mongodb-blog-app/models/index.js
@@ -1,4 +1,7 @@
 "use strict";
 module.exports = {
   User: require("./user"),
+  Post: require("./post"),
+  Comment: require("./comment"), // Add Comment model
+  Like: require("./like"), // Add Like model
 };
